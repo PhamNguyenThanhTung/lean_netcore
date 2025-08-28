cd19202db1d61ba07fb689103f1dae1f7185adb6
PhamNguyenThanhTung <148525922+PhamNguyenThanhTung@users.noreply.github.com>
2025-08-28 22:39:00 +0700

    First commit

---
+++ mvcdemo/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using mvcdemo.Models;
+
+namespace MvcDemoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Đây là trang Index của HomeController";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "Đây là trang About của HomeController";
+            return View();
+        }
+    }
+    
+}
---
+++ mvcdemo/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace MvcDemoApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult List()
+        {
+            ViewBag.Message = "Đây là danh sách sản phẩm (List Action)";
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            ViewBag.Message = "Đây là chi tiết sản phẩm (Detail Action)";
+            return View();
+        }
+    }
+}
