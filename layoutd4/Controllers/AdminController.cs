5e0058085d25b7c6eebafe6a434021a7af581864
PhamNguyenThanhTung <148525922+PhamNguyenThanhTung@users.noreply.github.com>
2025-09-04 22:10:28 +0700

    Clean up .vs, bin, obj and update lab03

---
+++ layoutd4/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BT03_adminPage.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Dashboard";
+            // Truyền dữ liệu động tương tự như JSP EL:
+            ViewData["countUsers"] = 123;
+            ViewData["countProducts"] = 456;
+            ViewData["countOrders"] = 789;
+            ViewData["LoggedInUser"] = "Tung"; // Tên người dùng đăng nhập
+            return View();
+        }
+    }
+}
